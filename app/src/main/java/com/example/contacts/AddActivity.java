1f7dd260e57950965db1e7141d790abedac0f7ce
vasilio13 <vasilkhaliaukin@gmail.com>
2020-06-09 13:02:39 +0300

    Abort second activity

---
+++ app/src/main/java/com/example/contacts/AddActivity.java
@@ -0,0 +1,92 @@
+package com.example.contacts;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        // Получаем сообщение из объекта intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.s);
+
+        // Создаем текстовое поле
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        Toolbar toolbarDone = (Toolbar) findViewById(R.id.toolbar);
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        final EditText numberMailET = findViewById(R.id.number_mail_et);
+        radioGroup.clearCheck();
+
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case -1:
+                        numberMailET.setText("");
+                        break;
+                    case R.id.radioButtonPhone:
+                        numberMailET.setHint("+375 29 xx - xx - xx");
+                        break;
+                    case R.id.radioButtonMail:
+                        numberMailET.setHint("name@hosting.zone");
+                        break;
+                }
+            }
+        });
+    }
+     @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        }
+
+       @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+           /**
+            * switch (item.getItemId()) {
+            * 		case R.id.menuPurchasesListNewRecord:
+            * 			// TODO: обработчик нажатия здесь
+            * 			return true;
+            *                }
+            *
+            * 		return super.onOptionsItemSelected(item);
+
+            *
+            */
+
+
+
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.toolbar_button_done) {
+                Toast.makeText(AddActivity.this, "Action clicked", Toast.LENGTH_LONG).show();
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+}
---
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.contacts;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private EditText newItemEditText;
+
+    public static String s;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(new NameListAdapter());
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+        newItemEditText = findViewById(R.id.newItemEditText);
+
+
+        FloatingActionButton addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newName = newItemEditText.getText().toString();
+                s=newName;
+
+              if (!newName.trim().isEmpty() && recyclerView.getAdapter() != null) {
+                   NameListAdapter adapter1 = (NameListAdapter) recyclerView.getAdapter();
+                   adapter1.addItem(newName);
+            }
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                intent.putExtra(s,s);
+               startActivity(intent);
+
+            }
+        });
+
+    }
+
+
+
+
+
+    static class NameListAdapter extends RecyclerView.Adapter<NameListAdapter.ItemViewHolder> {
+
+        private ArrayList<String> items = new ArrayList<>();
+
+        NameListAdapter() {
+        }
+
+        void addItem(String name) {
+            items.add(name);
+//            notifyItemChanged(items.indexOf(name)); // for item
+            notifyDataSetChanged(); // for all items
+        }
+
+        @NonNull
+        @Override
+        public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+            return new ItemViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+            holder.bindData(items.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return items != null ? items.size() : 0;
+        }
+
+        static class ItemViewHolder extends RecyclerView.ViewHolder {
+            private TextView nameText;
+
+            ItemViewHolder(@NonNull View itemView) {
+                super(itemView);
+                nameText = itemView.findViewById(R.id.nameText);
+            }
+
+            void bindData(String name) {
+                nameText.setText(name);
+            }
+        }
+    }
+}
